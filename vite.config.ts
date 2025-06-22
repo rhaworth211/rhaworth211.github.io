dc4d56c93ab91588a3ef9cc10468943283817570
Ryan Haworth <rhaworth211@msn.com>
2025-06-22 15:01:56 -0400

    Config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/rhaworth211.github.io/',
     plugins: [plugin()],
     server: {
         port: 65191,
