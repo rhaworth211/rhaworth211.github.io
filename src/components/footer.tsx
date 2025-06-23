5715206460e36bc900a278d0936831812ca124fe
rhaworth211 <rhaworth211@users.noreply.github.com>
2025-06-23 00:36:25 +0000

    deploy: 4b8eb212eda447740152c345d8197b30666e7e55

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="text-center py-6 text-sm text-blue-600">
-            &copy; {new Date().getFullYear()} Ryan Haworth
-        </footer>
-    );
-}
--- src/styles/cardStyle.ts
+++
@@ -1,9 +0,0 @@
-export const sharedCardSx = {
-    backgroundColor: 'rgba(200, 200, 200, 0.5)',
-    backdropFilter: 'blur(6px)',
-    WebkitBackdropFilter: 'blur(6px)',
-    boxShadow: 3,
-    mb: 2,
-    borderRadius: 2,
-    //color: 'rgba(226, 232, 240, 1)'
-};
